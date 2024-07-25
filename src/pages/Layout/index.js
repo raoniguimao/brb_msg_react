5ec96ed74288c3bda75e8f6d998e816983683d96
Raoni Alves Rocha <raoniguimao@gmail.com>
2024-07-25 18:53:34 -0300

    Adicionando layout e buscando usuário da sessão.

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,21 +1,26 @@
+import styles from './layout.module.css';
+
 import { Outlet } from "react-router-dom";
+
 import Cabecalho from "../../components/Cabecalho";
 import Rodape from "../../components/Rodape";
-import { useSessionStorage } from "usehooks-ts";
 
 const Layout = () => {
-    const [usuario, setUsuario, removeUsuario] = useSessionStorage('usuario', 'u844670');
+    const usuario = window.sessionStorage.getItem('usuario');
+
     return (
-        <div className="App container">
-            <header>
-                <Cabecalho />
-            </header>
-            <main>
-                <Outlet />
-            </main>
-            <footer>
-                <Rodape usuario={usuario} />
-            </footer>
+        <div className='container'>
+            <div className={styles.App}>
+                <header>
+                    <Cabecalho />
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+                <footer>
+                    <Rodape usuario={usuario} />
+                </footer>
+            </div>
         </div>
     );
 };
