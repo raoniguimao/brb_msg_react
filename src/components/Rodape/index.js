08b109d7f3c3e366ba94f8c207e4afdb0fbd4ced
Raoni Alves Rocha <raoniguimao@gmail.com>
2024-07-25 18:54:36 -0300

    Formatação da localidade

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -9,7 +9,7 @@ const Rodape = ({ usuario }) => {
 
     return (<>
         <div className="row">
-            <div className="col-4">Brasília - DF, {diasSemana[dtAtual.getDay()]}, {`${dia} de ${mes} de ${ano}`}.</div>
+            <div className="col-4">Brasília/DF, {diasSemana[dtAtual.getDay()]}, {`${dia} de ${mes} de ${ano}`}.</div>
             <div className="col-2"></div>
             <div className="col-2">{usuario}</div>
             <div className="col-4"></div>
