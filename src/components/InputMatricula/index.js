2d2cec2f930995811d3d6b1e388147fae36efe16
Raoni Alves Rocha <raoniguimao@gmail.com>
2024-07-25 18:51:53 -0300

    Corrigindo submissão de login

--- src/components/InputMatricula/index.js
+++ src/components/InputMatricula/index.js
@@ -1,18 +1,19 @@
 import InputMask from "react-input-mask";
 
-const InputMatricula = ({ label, nome, mascara, obrigatorio = false, placeHolder }) => {
-    return (<>
-        <label htmlFor={nome}>{label}:</label>
-        <InputMask
-            id={nome}
-            name={nome}
-            mask={mascara}
-            required={obrigatorio}
-            className="form-control"
-            placeholder={placeHolder}
-        >
-        </InputMask>
-    </>);
+const InputMatricula = ({ label, nome, mascara, obrigatorio = false, placeHolder, onChange }) => {
+    return (
+        <div>
+            <label htmlFor={nome}> {label}: </label>
+            <InputMask
+                id={nome} name={nome}
+                mask={mascara}
+                required={obrigatorio}
+                className="form-control" placeholder={placeHolder}
+                onChange={onChange}
+            >
+            </InputMask>
+        </div>
+    )
 }
 
 export default InputMatricula;
--- src/forms/Login/index.js
+++ src/forms/Login/index.js
@@ -1,22 +1,60 @@
 // import style from './Login.module.css';
 import InputMatricula from "../../components/InputMatricula";
 import FormButtons from '../../components/FormButtons';
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const FormLogin = () => {
+    const [inputs, setInputs] = useState('');
+    const navigate = useNavigate();
+
+    const aoEntrar = (evento) => {
+        evento.preventDefault();
+        window.sessionStorage.setItem('usuario', inputs.matricula);
+        navigate('/entrada');
+    }
+
+    const aoAlterar = (evento) => {
+        const name = evento.target.name;
+        const value = evento.target.value;
+
+        setInputs(values => ({ ...values, [name]: value }));
+    }
+
+    function logar(formData) { }
+
     return (
-        <form method="post">
+        <form onSubmit={evento => aoEntrar(evento)}>
             <div className="form-group row">
-                <InputMatricula label={"Matrícula"} nome={"matricula"} id={"matricula"} mascara={"u999999"} obrigatorio={true} placeHolder={"'u'+ matricula"} />
+                <InputMatricula
+                    label={"Matrícula"}
+                    nome={"matricula"}
+                    id={"matricula"}
+                    mascara={"u999999"}
+                    obrigatorio={true}
+                    placeHolder={"'u'+ matricula"}
+
+                    onChange={aoAlterar}
+                />
             </div>
             <div className="form-group row">
                 <label htmlFor="senha">Senha:</label>
-                <input type="password" name="senha" id={"senha"} className="form-control" required />
+                <input type="password" className="form-control"
+                    name="senha" id="senha"
+                    placeholder="senha"
+                    required
+
+                    onChange={aoAlterar}
+                />
             </div>
             <div className="form-group row">
                 <label htmlFor="perfil">Perfil:</label>
-                <select name="perfil" id="perfil" className="custom-select">
+                <select name="perfil" id="perfil"
+                    required className="custom-select"
+
+                    onChange={aoAlterar}>
                     <option></option>
-                    <option value='M'>Monitor</option>
+                    <option value='Monitor'>Monitor</option>
                 </select>
             </div>
             <div className="row btn-group">
