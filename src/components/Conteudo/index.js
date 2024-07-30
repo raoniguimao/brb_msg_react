96d95fa27df1d91a778c543c9f848fcbe31f8b3b
Raoni Alves Rocha <raoniguimao@gmail.com>
2024-07-30 09:17:33 -0300

    Componente para estruturar o conteúdo da página.

---
+++ src/components/Conteudo/index.js
@@ -0,0 +1,32 @@
+// import Hora from "./hora";
+import Atalhos from "../../components/Atalhos";
+
+const Conteudo = ({ children }) => {
+	return (
+		<>
+			<div className="row">
+				<h4>Barra de ações</h4>
+			</div>
+			<div className="row">
+				<div id="dHoraAtual">
+					{/* <Hora /> */}
+				</div>
+				<div id="dBreadcrumb">
+					<h4>Breadcrumb</h4>
+				</div>
+			</div>
+			<div className="row">
+				<Atalhos />
+				<section id="secArvore" className="col-2">
+					<h3>Árvore</h3>
+				</section>
+				<section id="secTelas" className="col-6">
+					<h3>Tela</h3>
+					{children}
+				</section>
+			</div>
+		</>
+	);
+}
+
+export default Conteudo;
