2543cbbf4293ffcedf6cd9b9c050f52e98b66e24
Raoni Alves Rocha <raoniguimao@gmail.com>
2024-07-30 09:17:59 -0300

    Chamada ao componente de conteúdo da página

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -2,8 +2,9 @@ import styles from './layout.module.css';
 
 import { Outlet } from "react-router-dom";
 
-import Cabecalho from "../../components/Cabecalho";
-import Rodape from "../../components/Rodape";
+import Cabecalho from "components/Cabecalho";
+import Rodape from "components/Rodape";
+import Conteudo from 'components/Conteudo';
 
 const Layout = () => {
     const usuario = window.sessionStorage.getItem('usuario');
@@ -15,7 +16,9 @@ const Layout = () => {
                     <Cabecalho />
                 </header>
                 <main>
-                    <Outlet />
+                    <Conteudo>
+                        <Outlet />
+                    </Conteudo>
                 </main>
                 <footer>
                     <Rodape usuario={usuario} />
