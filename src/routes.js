25c322b91da958774984b22339f38947a93a52e5
Raoni Alves Rocha <raoniguimao@gmail.com>
2024-08-02 16:25:51 -0300

    Inclusão de rota cadastros acesso usuário

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageLogin from './pages/Login';
 import Layout from "./pages/Layout";
 import Tela01 from "./pages/Tela01";
+import Usuario from "pages/Cadastros/Acesso/Usuario";
 
 function AppRoutes() {
     return (
@@ -10,6 +11,7 @@ function AppRoutes() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<PageLogin />} />
                     <Route path="/entrada" element={<Tela01 />} />
+                    <Route path="/Cadastros/Acesso/Usuario" element={<Usuario />} />
                 </Route>
             </Routes>
         </BrowserRouter>
